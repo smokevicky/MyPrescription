b635f366a4ee2d617fc3d6a9b390d5bfdeccf814
Jyoti Jena <tdv.jyoti@gmail.com>
2016-09-19 12:58:41 +0530

    Configured and added all the Entity Framework references to MyPerscription.EntityFramework

--- MyPrescription.DAL/SignUpDAL.cs
+++ MyPrescription.DAL/SignUpDAL.cs
@@ -5,10 +5,11 @@
 ** Purpose:  Adds a New User to the db
 ********************************************************/
 
-using MyPrescription.DAL.App_Code;
 using MyPrescription.Models;
 using MyPrescription.Util;
 using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MyPrescription.DAL
 {
@@ -24,71 +25,52 @@ namespace MyPrescription.DAL
         /// <returns></returns>
         public UserModel SignUp(UserModel userModelObject)
         {
-            //old code
-            //var userModelReturnObject = new UserModel();
-            //var sqlUtilityObject = new SqlUtility();
+            var userModelReturnObject = new UserModel();
+            var sqlUtilityObject = new SqlUtility();
 
-            //try
-            //{
-            //    var cmd = new SqlCommand("SignUp", sqlUtilityObject.con) { CommandType = CommandType.StoredProcedure };
-            //    cmd.Parameters.AddWithValue("@userId", userModelObject.userId);
-            //    cmd.Parameters.AddWithValue("@fname", userModelObject.firstName);
-            //    cmd.Parameters.AddWithValue("@lname", userModelObject.lastName);
-            //    cmd.Parameters.AddWithValue("@email", userModelObject.email);
-            //    cmd.Parameters.AddWithValue("@password", userModelObject.password);
-            //    sqlUtilityObject.con.Open();
-
-            //    SqlDataReader reader = cmd.ExecuteReader();
-            //    if (!reader.HasRows)
-            //    {
-            //        //isValid = 0 : Not successfully logged in
-            //        userModelReturnObject.statusCode = StatusCode.invalid;
-            //    }
-            //    else
-            //    {
-            //        while (reader.Read())
-            //        {
-            //            //isValid = 1 : User exists
-            //            userModelReturnObject.statusCode = StatusCode.valid;
-            //            userModelReturnObject.userId = userModelObject.userId;
-            //            userModelReturnObject.token = reader.GetValue(0).ToString();
-            //            userModelReturnObject.isActive = (bool)reader.GetValue(1);
-            //            userModelReturnObject.email = (string)reader.GetValue(2);
-            //            userModelReturnObject.status = (string)reader.GetValue(3);
-            //        }
-            //    }
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    //isValid = 2 : Error
-            //    userModelReturnObject.statusCode = StatusCode.error;
-            //    userModelReturnObject.error = ex.ToString();
-            //}
-            //finally
-            //{
-            //    sqlUtilityObject.con.Close();
-            //}
-
-            //return userModelReturnObject;
-
-            //new code
-            UserModel userModelobject = new UserModel();
             try
             {
-                using (var mpe = new MyPrescriptionEntities())
+                var cmd = new SqlCommand("SignUp", sqlUtilityObject.con) { CommandType = CommandType.StoredProcedure };
+                cmd.Parameters.AddWithValue("@userId", userModelObject.userId);
+                cmd.Parameters.AddWithValue("@fname", userModelObject.firstName);
+                cmd.Parameters.AddWithValue("@lname", userModelObject.lastName);
+                cmd.Parameters.AddWithValue("@email", userModelObject.email);
+                cmd.Parameters.AddWithValue("@password", userModelObject.password);
+                sqlUtilityObject.con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
                 {
-                    userModelobject.statusCode = StatusCode.valid;
-                    mpe.SignUpFromCode(000000001, "testFname", "testLname", "testemail@test.com", "testpass");
+                    //isValid = 0 : Not successfully logged in
+                    userModelReturnObject.statusCode = StatusCode.invalid;
+                }
+                else
+                {
+                    while (reader.Read())
+                    {
+                        //isValid = 1 : User exists
+                        userModelReturnObject.statusCode = StatusCode.valid;
+                        userModelReturnObject.userId = userModelObject.userId;
+                        userModelReturnObject.token = reader.GetValue(0).ToString();
+                        userModelReturnObject.isActive = (bool)reader.GetValue(1);
+                        userModelReturnObject.email = (string)reader.GetValue(2);
+                        userModelReturnObject.status = (string)reader.GetValue(3);
+                    }
                 }
             }
+
             catch (Exception ex)
             {
-                userModelobject.statusCode = StatusCode.error;
-                userModelObject.error = ex.ToString();
+                //isValid = 2 : Error
+                userModelReturnObject.statusCode = StatusCode.error;
+                userModelReturnObject.error = ex.ToString();
+            }
+            finally
+            {
+                sqlUtilityObject.con.Close();
             }
 
-            return userModelobject;
+            return userModelReturnObject;
         }
     }
 }
--- MyPrescription.EntityFramework/Class1.cs
+++ MyPrescription.EntityFramework/Class1.cs
@@ -2,6 +2,22 @@
 {
     public class Class1
     {
+        //        //new code
+        //        UserModel userModelobject = new UserModel();
+        //            try
+        //            {
+        //                using (var mpe = new MyPrescriptionEntities())
+        //                {
+        //                    userModelobject.statusCode = StatusCode.valid;
+        //                    mpe.SignUpFromCode(000000001, "testFname", "testLname", "testemail@test.com", "testpass");
+        //                }
+        //}
+        //            catch (Exception ex)
+        //            {
+        //                userModelobject.statusCode = StatusCode.error;
+        //                userModelObject.error = ex.ToString();
+        //            }
 
+        //            return userModelobject;
     }
 }
